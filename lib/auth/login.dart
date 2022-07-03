be51269735fdb7a60b41ca8b05875f7174a83e2d
Your Name <you@example.com>
2022-07-03 21:01:45 +0100

    tre

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: avoid_print
 
+import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -11,6 +12,67 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return const Text('Bonjour');
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const SizedBox(height: 10,) ,
+          Center(child: Image.asset('images/logo.jpg')),
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: Form(
+                child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                      hintText: "Email",
+                      prefixIcon: Icon(Icons.person),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(width: 1),
+                      )),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                      hintText: "Mot de passe",
+                      prefixIcon: Icon(Icons.person),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(width: 1),
+                      )),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: 
+                 [
+                    const Text('Si vous étes pas inscris ,'),
+                    InkWell(
+                      onTap:() {
+                        
+                      },
+                      
+                      child: const Text('Click ici !',style: TextStyle(color: Colors.blue),),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 10,),
+                ElevatedButton(
+                
+                  child: const Text(
+                    'Connexion',
+                    style: TextStyle(fontSize: 14),
+                  ),
+                  onPressed: () => {print('logeed')},
+                )
+              ],
+            )),
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/auth/signup.dart
+++ lib/auth/signup.dart
@@ -1,15 +1,86 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 
-class SignUp extends StatefulWidget {
-  const SignUp({Key? key}) : super(key: key);
+class Signup extends StatefulWidget {
+  const Signup({Key? key}) : super(key: key);
 
   @override
-  State<SignUp> createState() => _SignUpState();
+  State<Signup> createState() => _SignupState();
 }
 
-class _SignUpState extends State<SignUp> {
+class _SignupState extends State<Signup> {
   @override
   Widget build(BuildContext context) {
-    return   Container(child : const Text('Signup'));
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const SizedBox(height: 10,) ,
+          Center(child: Image.asset('images/logo.jpg')),
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: Form(
+                child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                      hintText: "Email",
+                      prefixIcon: Icon(Icons.person),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(width: 1),
+                      )),
+                ),
+                 TextFormField(
+                  decoration: const InputDecoration(
+                      hintText: "Nom Compet",
+                      prefixIcon: Icon(Icons.person),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(width: 1),
+                      )),
+                )
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                      hintText: "Mot de passe",
+                      prefixIcon: Icon(Icons.person),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(width: 1),
+                      )),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: 
+                 [
+                    const Text('Si vous étes pas inscris ,'),
+                    InkWell(
+                      onTap:() {
+                        
+                      },
+                      
+                      child: const Text('Click ici !',style: TextStyle(color: Colors.blue),),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 10,),
+                ElevatedButton(
+                
+                  child: const Text(
+                    'Créer Compte',
+                    style: TextStyle(fontSize: 14),
+                  ),
+                  onPressed: () => {print('logeed')},
+                )
+              ],
+            )),
+          )
+        ],
+      ),
+    );
   }
 }
